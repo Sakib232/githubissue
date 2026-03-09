c04c24d623c6f1c61619628e1b39c1ea0ad71bdf
sakib ahamed <sakibahamed33@gmail.com>
2026-03-09 21:12:27 +0600

    former function done

--- home.js
+++ home.js
@@ -63,3 +63,89 @@ function filterIssues(issues, status) {
   if (status === "all") return issues;
   return issues.filter((issue) => issue.status?.toLowerCase() === status);
 }
+
+function setActiveTab(activeStatus) {
+  tabButtons.forEach((btn) => {
+    if (btn.dataset.status === activeStatus) {
+      btn.className =
+        "tab-btn bg-violet-600 text-white px-5 py-2 rounded-lg text-sm font-medium";
+    } else {
+      btn.className =
+        "tab-btn bg-gray-100 text-gray-600 px-5 py-2 rounded-lg text-sm font-medium";
+    }
+  });
+}
+
+
+function getBorderColor(status) {
+  return status?.toLowerCase() === "open"
+    ? "border-t-[#20C997]"
+    : "border-t-[#B26BFF]";
+}
+
+function getStatusBadge(status) {
+  const currentStatus = status?.toLowerCase();
+
+  if (currentStatus === "open") {
+    return `
+      <span class="inline-flex items-center gap-1 text-[10px] font-semibold px-2 py-1 rounded-full bg-green-50 text-green-600 border border-green-100">
+        <span class="w-2 h-2 rounded-full bg-green-500"></span>
+        OPEN
+      </span>
+    `;
+  }
+
+  return `
+    <span class="inline-flex items-center gap-1 text-[10px] font-semibold px-2 py-1 rounded-full bg-violet-50 text-violet-600 border border-violet-100">
+      <span class="w-2 h-2 rounded-full bg-violet-500"></span>
+      CLOSED
+    </span>
+  `;
+}
+
+function getPriorityBadge(priority) {
+  const p = priority?.toLowerCase();
+
+  if (p === "high") {
+    return `<span class="text-[9px] font-semibold px-3 py-1 rounded-full bg-[#FDECEC] text-[#F05D5E]">HIGH</span>`;
+  }
+
+  if (p === "medium") {
+    return `<span class="text-[9px] font-semibold px-3 py-1 rounded-full bg-[#FFF4D6] text-[#D9A400]">MEDIUM</span>`;
+  }
+
+  if (p === "low") {
+    return `<span class="text-[9px] font-semibold px-3 py-1 rounded-full bg-[#F1F3F5] text-[#9AA1A9]">LOW</span>`;
+  }
+
+  return `<span class="text-[9px] font-semibold px-3 py-1 rounded-full bg-[#F1F3F5] text-[#9AA1A9]">N/A</span>`;
+}
+
+function getLabelBadges(label) {
+    if (!label || label.toLowerCase() === "no label") {
+        return `
+            <span class="text-[9px] font-semibold px-2 py-1 rounded-full bg-[#F4F5F7] text-[#9AA1A9]"># NO LABEL</span>
+        `;
+    }
+
+    const l = label.toLowerCase();
+
+    if (l === "bug") {
+        return `<span class="text-[9px] font-semibold px-2 py-1 rounded-full bg-[#FDECEC] text-[#E96A6A]"># BUG</span>`;
+    }
+
+    if (l === "enhancement") {
+        return `<span class="text-[9px] font-semibold px-2 py-1 rounded-full bg-[#E9FBF1] text-[#19B36B]"># ENHANCEMENT</span>`;
+    }
+
+    if (l === "help wanted") {
+        return `<span class="text-[9px] font-semibold px-2 py-1 rounded-full bg-[#FFF4D6] text-[#D9A400]"># HELP WANTED</span>`;
+    }
+
+    return `<span class="text-[9px] font-semibold px-2 py-1 rounded-full bg-[#F4F5F7] text-[#9AA1A9]"># ${label.toUpperCase()}</span>`;
+}
+
+function formatDate(dateString) {
+  if (!dateString) return "N/A";
+  return new Date(dateString).toLocaleDateString();
+}
